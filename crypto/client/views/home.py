164195dee15c95040f04b7368eed1fae5edc9d50
SefvandenHeurik <Sef.van.den.Heurik@student.gildeopleidingen.nl>
2024-12-05 14:14:45 +0100

    .

--- crypto/client/views/home.py
+++ crypto/client/views/home.py
@@ -6,8 +6,16 @@ from ..models import *
 
 @login_required
 def home(request):
+<<<<<<< Updated upstream
     template = "home.html"
     stef = "igorneuker"
+=======
+    # Fetch trending tokens from CoinGecko API
+
+    top_coins = get_highest_coins()
+    trending_tokens = get_trending_tokens()
+    template = "home/home.html"
+>>>>>>> Stashed changes
     context = {
     }
     return render(request, template, context)
